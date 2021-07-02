40e5a22399d701133ba7ab78d05072ca72f3c046
thewolfgames <thewolfgames@yahoo.com>
2021-07-02 19:17:39 +0300

    PS4JB

--- MakeCache75x.bat
+++ MakeCache75x.bat
@@ -1,12 +1,12 @@
 @echo off
 echo CACHE MANIFEST > test0.txt
-echo # v1.0 : %date% >> test0.txt
+echo # v2.5.5 Self-Host >> test0.txt
 echo. >> test0.txt
+
 set LOC=%~dp0
 
 dir /B /S /A:-D >> test0.txt
 
-echo. >> test0.txt
 findstr /v "media .bat .exe .mp4 .git .py restore New folder offlineCache75x.manifest test0.txt" test0.txt > test.txt
 del test0.txt
 
