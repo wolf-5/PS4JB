b8f58b4692729a475c8fb47bcdf2039f779cde37
THE WOLF HACK <70449764+wolf-5@users.noreply.github.com>
2021-11-21 20:32:30 +0200

    ps4jb

--- jb.js
+++ jb.js
@@ -138570,3 +138570,7 @@ var printf_ans = read_mem_as_string(printf_buf, printf_buf_end-printf_buf);
 var _ = malloc_nogc.pop();
 var _ = malloc_nogc.pop();
 var _ = malloc_nogc.pop();
+if(main_ret == 179 || main_ret == 0){
+} else{
+	alert("Jailbreak Failed!! Please Restart and Start Fresh!!!");
+}
